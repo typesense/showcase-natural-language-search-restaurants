1efee07902116e202f6a1eaa14e8379094376249
Hayden Hung Hoang <hayden.hhoang@gmail.com>
2025-07-05 12:26:42 +0700

    refine prompt

--- scripts/createModel.ts
+++ scripts/createModel.ts
@@ -22,7 +22,9 @@ E.g: open_hours.{day:=Mon && close:>=11}
 
 To search within a Radius use this syntax in the filter_by: geopoint:(lat, long, X km). You can also use miles "mi".
 E.g: geopoint:(48.90615915923891, 2.3435897727061175, 2 mi)
-The user's location will be embedded in the query in case they want to find restaurant near them (always remove it from your query after processing): USER:lat,long
+The user's location will always be embedded in the query in case they want to find restaurant near them (always remove it from your query after processing): USER:lat,long
+Use this user location for geosearch only if the query contains phrases like "near me".
+An user might ask to find restaurant near a place, in that case, use your knowlegde about the geolocation of that place.
 `,
       };
 
@@ -30,19 +32,21 @@ The user's location will be embedded in the query in case they want to find rest
       await typesense({ isServer: true })
         .nlSearchModels(MODEL_ID)
         .update(updatedConfig);
-      return;
+    } else {
+      console.log(
+        `Model with id: ${MODEL_ID} not found. Creating a new model...`
+      );
+      await typesense({ isServer: true }).nlSearchModels().create({
+        id: MODEL_ID,
+        model_name: 'google/gemini-2.5-flash',
+        api_key: GOOGLE_AI_STUDIO_API_KEY,
+        max_bytes: 16000,
+        temperature: 0.0,
+      });
     }
   } catch (err) {
-    console.log(
-      `Model with id: ${MODEL_ID} not found. Creating a new model...`
-    );
+    console.log(err);
+    return;
   }
-
-  await typesense({ isServer: true }).nlSearchModels().create({
-    id: MODEL_ID,
-    model_name: 'google/gemini-2.5-flash',
-    api_key: GOOGLE_AI_STUDIO_API_KEY,
-    max_bytes: 16000,
-    temperature: 0.0,
-  });
+  console.log('Success!');
 })();
